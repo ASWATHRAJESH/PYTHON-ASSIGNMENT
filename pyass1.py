e547f663f1664003f9dc772b2b03105a7914834b
ASWATHRAJESH <126188594+ASWATHRAJESH@users.noreply.github.com>
2023-03-02 21:33:16 +0530

    DAY 3 ASSIGNMENT COMPLETED

---
+++ pyass1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+P = float(input("Enter the Principal Amount : "))
+T = int(input("Enter the Time : "))
+R = float(input("Enter the Rate : "))
+
+SI =  (P * T * R)/100
+          
+print("\nSimple Interest :",SI)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
