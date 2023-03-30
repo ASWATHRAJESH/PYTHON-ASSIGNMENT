0aac20d600a5fdc513fddd84030b32bc3fc568fa
ASWATHRAJESH <126188594+ASWATHRAJESH@users.noreply.github.com>
2023-03-30 21:10:23 +0530

    DAY 3 ASSIGNMENT COMPLETED

---
+++ minor_or_adult_or_senior.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+age = int(input("Enter your age : "))
+
+if(age < 18):
+    print("You are a minor")
+elif(age >= 18 and age < 65):
+    print("You are an adult")
+else:
+    print("You are a senior")
+
+
+# In[2]:
+
+
+age = int(input("Enter your age : "))
+
+if(age < 18):
+    print("You are a minor")
+elif(age >= 18 and age < 65):
+    print("You are an adult")
+else:
+    print("You are a senior")
+
+
+# In[3]:
+
+
+age = int(input("Enter your age : "))
+
+if(age < 18):
+    print("You are a minor")
+elif(age >= 18 and age < 65):
+    print("You are an adult")
+else:
+    print("You are a senior")
+
+
+# In[ ]:
+
+
+
+
