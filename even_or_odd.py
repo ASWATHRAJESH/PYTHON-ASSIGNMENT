e7dd659e6a42d461e72d249a171eaab165474192
ASWATHRAJESH <126188594+ASWATHRAJESH@users.noreply.github.com>
2023-04-28 19:30:45 +0530

    DAY 2 ASSIGNMENT COMPLETED

---
+++ even_or_odd.py
@@ -0,0 +1,8 @@
+def even_or_odd(n) :
+    if(n%2== 0) :
+        print("Entered number",n,"is even")
+    else :
+        print("Entered number",n,"is odd")
+
+n = int(input("Enter a number : "))        
+even_or_odd(n)
